ebb4703ce1eb7a435dee25af97d236bb645de4d6
s20171106577 <s20171106577@123.local>
2018-11-28 09:49:21 +0800

    Initial Commit

---
+++ newcaculaterTests/newcaculaterTests.swift
@@ -0,0 +1,34 @@
+//
+//  newcaculaterTests.swift
+//  newcaculaterTests
+//
+//  Created by s20171106577 on 2018/11/28.
+//  Copyright © 2018 s20171106577. All rights reserved.
+//
+
+import XCTest
+@testable import newcaculater
+
+class newcaculaterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
